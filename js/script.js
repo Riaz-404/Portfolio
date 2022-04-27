15bf4633d6d0432e2f359eef5030753512009228
Riaz-404 <imriaz.cu@gmail.com>
2022-04-27 23:47:42 +0600

    done

---
+++ js/script.js
@@ -0,0 +1,119 @@
+AOS.init({
+    duration: 600
+});
+
+
+(function ($) {
+    'use strict';
+
+
+    // post gallery
+
+    $('.post_gallery').owlCarousel({
+        loop: true,
+        margin: 1,
+        nav: true,
+        dots: false,
+        navText: ["<i class='ti-angle-left'></i>", "<i class='ti-angle-right'></i>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+
+    // SCROLL TO TOP
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 70) {
+            $('.backtop').addClass('reveal');
+        } else {
+            $('.backtop').removeClass('reveal');
+        }
+    });
+
+    // Sticky Menu
+    // 
+    $(window).on('scroll', function () {
+        if ($('.navigation').offset().top > 100) {
+            $('.navigation').addClass('fixed-nav');
+        } else {
+            $('.navigation').removeClass('fixed-nav');
+        }
+    });
+
+    /* Closes the Responsive Menu on Menu Item Click*/
+    $('.navbar-collapse .navbar-nav a').on('click', function () {
+        $('.navbar-toggler:visible').click();
+    });
+
+
+
+    $('a.smoth-scroll').on('click', function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top - 50
+        }, 1000);
+        e.preventDefault();
+    });
+
+
+    $('.testimonial-slider').slick({
+        slidesToShow: 1,
+        infinite: true,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+
+    $(".text-rotating").Morphext({
+
+        // The [in] animation type. Refer to Animate.css for a list of available animations.
+        animation: "bounceIn",
+        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+        separator: ",",
+        // The delay between the changing of each phrase in milliseconds.
+        speed: 4000,
+        complete: function () {
+            // Called after the entrance animation is executed.
+        }
+    });
+
+
+
+    $(window).on('load', function () { // makes sure the whole site is loaded
+
+        // ----------------------- 
+        // Progress Bar--------------------
+        // 
+        $('.progress-bar').each(function () {
+            var width = $(this).data('percent');
+            $(this).css({ 'transition': 'width 3s' });
+            $(this).appear(function () {
+                console.log('hello');
+                $(this).css('width', width + '%');
+                $(this).find('.count').countTo({
+                    from: 0,
+                    to: width,
+                    speed: 3000,
+                    refreshInterval: 50
+                });
+            });
+        });
+
+
+    });  //End On Load Function
+
+
+
+    var myCarousel = document.querySelector('#carouselExampleIndicators')
+    var carousel = new bootstrap.Carousel(myCarousel)
+
+})(jQuery);
